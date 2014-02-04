a93605a1c010029605371a9448e7ff66bf61fde7
Hanson Walker <hwalker091@gmail.com>
2014-02-04 17:41:30 -0600

    set up nav menu and added additional pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Agency5App::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
+  get "work" => "pages#work"
+  get "contact" => "pages#contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
