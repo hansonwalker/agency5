d85f504237b08e457b9a34b9a10db3a120fb339d
Hanson Walker <hwalker091@gmail.com>
2014-02-04 16:46:20 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Agency5App::Application.config.secret_key_base = '0d1ac62f28ada3aa9f71e1c9490f614f2fe6a7a5888f110c97ef75684eba1b2750d1083c7961bccc51e182e20636ffe83fb5cd683dd6d9bdd34963e971c0a73a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Agency5App::Application.config.session_store :cookie_store, key: '_agency5_app_session'
